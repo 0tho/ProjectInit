504adc6ed6295c8ddc543c3d16c04a36d73e0cb8
Otho <marcelo.lopes.lotufo@gmail.com>
2016-12-30 01:52:18 -0200

    Fix some issues with path in different plataforms

--- script2.py
+++ script2.py
@@ -2,19 +2,18 @@ def url(path):
     print(path[0:4])
     if path[0:4] == "http":
         return 1
-    else:    
+    else:
         return 0
 
 if __name__ == "__main__":
-   
-    import sys    
+
+    import sys
     import os
     import urllib.request
     import shutil
 
     print (sys.argv[1])
 
-	
     chavesAbertas = 0
     colchetesAbertos = 0
     aspasAbertas = 0
@@ -26,20 +25,22 @@ if __name__ == "__main__":
     variavel = ""
     valor = ""
     moverPasta = 0
-    dataInFile = 0    
-    referencia = 0  
+    dataInFile = 0
+    referencia = 0
 
     variaveis = []
-	
+
     f = open(sys.argv[1], 'r')
     endereco = f.readline()
-    endereco = endereco.split("\n")[0]
+    endereco = endereco.replace("\r", "")
+    endereco = endereco.replace("\n", "")
+
     os.chdir(endereco)
 
     texto = f.read()
     f.close()
 
-	
+
     for letra in texto:
         if dataInFile:
             if letra == '"':
@@ -55,7 +56,7 @@ if __name__ == "__main__":
                 chavesAbertas-=1
             elif letra == '"':
                 dataInFile = 1
-	
+
     if colchetesAbertos != 0 or chavesAbertas != 0 or dataInFile:
         print("Invalido")
     else:
@@ -65,47 +66,47 @@ if __name__ == "__main__":
                 if letra == ']':
                     valor = buffer
                     if(url(valor)):
-                        urllib.request.urlretrieve(valor, (os.getcwd()+"\\"+variavel))
+                        urllib.request.urlretrieve(valor, os.path.join(os.getcwd(), variavel))
                     else:
-                        valor = valor.replace("/", "\\")
-                        os.system ("copy %s %s" % (valor, (os.getcwd()+"\\"+variavel)))                        
+                        valor = os.path.normpath(valor)
+                        os.system ("copy %s %s" % (valor, os.path.join(os.getcwd(), variavel)))
                     buffer = ""
                     referencia = 0
                 else:
                     buffer += letra
-            elif aspasAbertas == 0:            
+            elif aspasAbertas == 0:
                 if letra == '"':
                     f = open(variavel, 'w')
-                    aspasAbertas = 1                   
+                    aspasAbertas = 1
                 elif letra == ":":
                     variavel = buffer
+                    variavel = variavel.replace("\r", "")
                     variavel = variavel.replace("\n", "")
                     variavel = variavel.replace("\t", "")
                     variavel = variavel.replace(" ", "")
                     buffer = ""
                 elif letra == ",":
                     buffer = buffer
-                elif letra == "{":                
-                    moverPasta = +1                
+                elif letra == "{":
+                    moverPasta = +1
                     os.system('mkdir '+variavel)
-                    os.chdir(variavel)                
+                    os.chdir(variavel)
                 elif letra == "}":
-                    moverPasta = -1 
+                    moverPasta = -1
                 elif letra == "[":
                     buffer = ""
                     referencia = 1
                 elif letra != "\n" or letra!=" " or letra !="\t":
-                    buffer += letra                    
+                    buffer += letra
                 if moverPasta == -1:
-                    os.chdir(os.getcwd()+"/..")
+                    os.chdir( os.path.join(os.getcwd(), ".."))
                     print(os.getcwd())
                     moverPasta = 0
-            elif letra == '"':                
+            elif letra == '"':
                 aspasAbertas =0
                 valor = buffer
                 f.write(valor)
-                f.close()               
+                f.close()
                 buffer = ""
             else:
                 buffer += letra
-
